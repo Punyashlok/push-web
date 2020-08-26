25bf69019ae878a2c2451b6ba249f7a1e470d03a
Dash <dash@famil.ca>
2020-08-25 19:19:50 -0700

    Trial for multiple subscribers

--- push.js
+++ push.js
@@ -9,14 +9,16 @@ push.setVapidDetails('mailto:punyashlok.com@gmail.com', vapidKeys.publicKey, vap
 
 //Pull out from db of stored/registered people who want to receive notifications. Its the result of pushManager.subscribe in index.html
 
-let sub = [{"endpoint":"https://fcm.googleapis.com/fcm/send/d54A-Qnn7SE:APA91bEB00lpRL5QnRJ7bITeSWmdf3NxsiMS5bf4Xd2UCdNN8Lx-Jb2wakRb1oV80aLYBsgsttrX4u7ehBFRAYg0KqsktaQAvMH69nvA938PsQXcijS3NC4sCD6E-2XSXPDQrEOZD08u",
+let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/d54A-Qnn7SE:APA91bEB00lpRL5QnRJ7bITeSWmdf3NxsiMS5bf4Xd2UCdNN8Lx-Jb2wakRb1oV80aLYBsgsttrX4u7ehBFRAYg0KqsktaQAvMH69nvA938PsQXcijS3NC4sCD6E-2XSXPDQrEOZD08u",
              "expirationTime":null,
-             "keys":{"p256dh":"BIBZWrXIcqotT7uaFMfyiW9R1Bf28GLzYKtnD94Ksgq3mV_3GfKLgsIiXUUJXrrGHaJ31CxJNB8xTwFDKoIrbDs","auth":"IEoo8MDeOTkC_dsVmkSuAg"}},
+             "keys":{"p256dh":"BIBZWrXIcqotT7uaFMfyiW9R1Bf28GLzYKtnD94Ksgq3mV_3GfKLgsIiXUUJXrrGHaJ31CxJNB8xTwFDKoIrbDs","auth":"IEoo8MDeOTkC_dsVmkSuAg"},
 
-           {"endpoint":"https://fcm.googleapis.com/fcm/send/fmZARFRVQrY:APA91bFdxdEieVhlgarwPj_13l5YM91r_ZPw0X2txINObQeIoK2zrclsEknWeXGl5ITe3PQ-0RwGMDl4muwB1PGQbp-bbYms96ezPeCSbf9WH_asLc-vXp6qDDOm7ohClQlkbclOeD4N",
+           "endpoint":"https://fcm.googleapis.com/fcm/send/fmZARFRVQrY:APA91bFdxdEieVhlgarwPj_13l5YM91r_ZPw0X2txINObQeIoK2zrclsEknWeXGl5ITe3PQ-0RwGMDl4muwB1PGQbp-bbYms96ezPeCSbf9WH_asLc-vXp6qDDOm7ohClQlkbclOeD4N",
             "expirationTime":null,
-            "keys":{"p256dh":"BJEpsF3qk6Rl8XWPu1Os0igNToeL_4oxc_6wf3KNo_nh6JhKvRnAiMhf6NwPvczCbFZ11L3n5Xbi6hFpEZUKjDA","auth":"RQwKkpU6WYtg7GI6CS_PCQ"}}
-          ];
+             "keys":{"p256dh":"BJEpsF3qk6Rl8XWPu1Os0igNToeL_4oxc_6wf3KNo_nh6JhKvRnAiMhf6NwPvczCbFZ11L3n5Xbi6hFpEZUKjDA","auth":"RQwKkpU6WYtg7GI6CS_PCQ"}
+          };
+
+
 
 
  push.sendNotification(sub,'Hello World');
