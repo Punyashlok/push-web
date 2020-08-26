75182b117db50c98f9e8b3e13ea3c2d1b361c77b
Dash <dash@famil.ca>
2020-08-25 19:53:27 -0700

    Testing with pushManager subscription

--- push.js
+++ push.js
@@ -9,6 +9,17 @@ push.setVapidDetails('mailto:punyashlok.com@gmail.com', vapidKeys.publicKey, vap
 
 //Pull out from db of stored/registered people who want to receive notifications. Its the result of pushManager.subscribe in index.html
 
+
+async function subscribe (){
+            let sw = await navigator.serviceWorker.ready;
+            let push = await sw.pushManager.subscribe({
+                userVisibleOnly:true,
+                applicationServerKey:'BKB4N-zY4mSuEscN1GQ62dpLZ4gndWGX9OSrsdx413Wk7vcHmH5wCTT6k9yHxzylpnMEHRb0gOYHEFv1xKoQ-ZM'
+            })
+            console.log(JSON.stringify(push));
+            console.log(JSON.stringify(push.keys));
+}
+
 let sub1 = {"endpoint":"https://fcm.googleapis.com/fcm/send/d54A-Qnn7SE:APA91bEB00lpRL5QnRJ7bITeSWmdf3NxsiMS5bf4Xd2UCdNN8Lx-Jb2wakRb1oV80aLYBsgsttrX4u7ehBFRAYg0KqsktaQAvMH69nvA938PsQXcijS3NC4sCD6E-2XSXPDQrEOZD08u",
              "expirationTime":null,
              "keys":{"p256dh":"BIBZWrXIcqotT7uaFMfyiW9R1Bf28GLzYKtnD94Ksgq3mV_3GfKLgsIiXUUJXrrGHaJ31CxJNB8xTwFDKoIrbDs","auth":"IEoo8MDeOTkC_dsVmkSuAg"}};
