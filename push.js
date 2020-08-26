bd30500806e574524827fcbe9c75b5f261713327
Dash <dash@famil.ca>
2020-08-25 18:11:48 -0700

    Chnages to sw.js

--- push.js
+++ push.js
@@ -7,6 +7,11 @@ let vapidKeys = { publicKey: 'BKB4N-zY4mSuEscN1GQ62dpLZ4gndWGX9OSrsdx413Wk7vcHmH
 
 push.setVapidDetails('mailto:punyashlok.com@gmail.com', vapidKeys.publicKey, vapidKeys.privateKey);
 
-let sub = {};  //Pull out from db of stored/registered people who want to receive notifications. Its the result of pushManager.subscribe in index.html
+//Pull out from db of stored/registered people who want to receive notifications. Its the result of pushManager.subscribe in index.html
 
-push.sendNotification(sub,'Hello World');
+let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/fmZARFRVQrY:APA91bFdxdEieVhlgarwPj_13l5YM91r_ZPw0X2txINObQeIoK2zrclsEknWeXGl5ITe3PQ-0RwGMDl4muwB1PGQbp-bbYms96ezPeCSbf9WH_asLc-vXp6qDDOm7ohClQlkbclOeD4N",
+           "expirationTime":null,
+           "keys":{"p256dh":"BJEpsF3qk6Rl8XWPu1Os0igNToeL_4oxc_6wf3KNo_nh6JhKvRnAiMhf6NwPvczCbFZ11L3n5Xbi6hFpEZUKjDA",
+                    "auth":"RQwKkpU6WYtg7GI6CS_PCQ"}}
+
+ push.sendNotification(sub,'Hello World');
--- sw.js
+++ sw.js
@@ -1,3 +1,18 @@
-self.addEventListener('push', () => {
-    self.registration.sendNotification('Hello World', {});
+self.addEventListener('push', (e) => {
+    //self.registration.sendNotification('Hello World', {});
+
+    var options = {
+        body:'Notification generated from a push !',
+        icon:'images/example.png',
+        vibrate:[100,50,100],
+        data:{
+            dateOfArrival: Date.now(),
+            primaryKey: 2
+        },
+        actions: [
+            {action: 'explore', title: 'Explore web-push', icon:'https://cdn2.iconfinder.com/data/icons/mixed-rounded-flat-icon/512/megaphone-64.png'},
+            {action: 'close', title: 'close', icon:'images/xmark.png'}
+        ]
+    };
+    e.waitUntil(self.registration.showNotification('Hello World',options));
 })
